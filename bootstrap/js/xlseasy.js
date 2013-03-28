279073f4b319b46b64e19fac4b5b030c7ee56f0b
Marius Guede <mrius>
2013-03-28 11:29:29 +0100

    update website

--- bootstrap/js/xlseasy.js
+++ bootstrap/js/xlseasy.js
@@ -1,3 +1,22 @@
+/* ===================================================
+ * xlseasy.js v1.0.0
+ * http://adorsys.github.com/xlseasy/
+ * ===================================================
+ * Copyright 2013 adorsys GmbH & Co. KG.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ========================================================== */
+
 $(document).ready(function() {
 	
 	// event: click on adorsys-logo
@@ -17,11 +36,10 @@ $(document).ready(function() {
 		$(targetedId).slideToggle("slow");
 		
 		// loop to hide all div.example-details except the targeted one
-		for (var i = 1; i <= 3; i++) {
+		for (var i = 0; i < 3; i++) {
 			var loopID = 'exp-' + i;
 			if (loopID != id) $('div.example-details[id="' + loopID + '"]').css('display', 'none');
 		}
-
 		return false;
 	});
 });
