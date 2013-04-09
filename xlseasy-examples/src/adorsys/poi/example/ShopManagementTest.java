ab2abaeeebe044d1dc6f61851d25cea9a5c479ed
Marius Guede <mrius>
2013-04-09 14:52:45 +0200

    ShopManagementTest.java

--- xlseasy-examples/src/adorsys/poi/example/ShopManagementTest.java
+++ xlseasy-examples/src/adorsys/poi/example/ShopManagementTest.java
@@ -10,12 +10,6 @@ import org.apache.poi.hssf.usermodel.HSSFSheet;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.ss.usermodel.Sheet;
 
-import adorsys.poi.example.Client;
-import adorsys.poi.example.Product;
-import adorsys.poi.example.ShopManagement;
-import adorsys.poi.example.MyExcelFunction;
-
-
 /**
  * Simulates the management of a shop using our implemented classes (Client,
  * Product, ShopManagement, MyExcelFunction).
@@ -40,15 +34,16 @@ public class ShopManagementTest extends MyExcelFunction {
 	 * creates a spreadsheet and records data into it.
 	 */
 	public void createExcel() {
-		
+
 		try {
 			// creates the workbook
 			HSSFWorkbook book = new HSSFWorkbook();
 
 			// creates the sheet for this book
 			HSSFSheet sheet = book.createSheet("1st Sheet");
-			
-			// the following line allow to create a sheet and add sheet's name with special characters such as [], ? # + %$¤"
+
+			// the following line allow to create a sheet and add sheet's name
+			// with special characters such as [], ? # + %$¤"
 			// book.createSheet(WorkbookUtil.createSafeSheetName("?789.[]uyfa^!$#*%"));
 
 			// creates row and cell
@@ -56,17 +51,19 @@ public class ShopManagementTest extends MyExcelFunction {
 			HSSFCell cell;
 
 			// creates 3 clients
-			Client client1 = new Client("Siegfried Mueller", "Bahnhofplatz 1", 90489,
-					"Nuremberg", "Germany");
-			Client client2 = new Client("Antoine David", "Rue des Joffres 34", 20489,
-					"Marseille", "France");
-			Client client3 = new Client("James Houston", "New-Town Way 10", 10489,
-					"Los Angeles", "USA");
+			Client client1 = new Client("Siegfried Mueller", "Bahnhofplatz 1",
+					90489, "Nuremberg", "Germany");
+			Client client2 = new Client("Antoine David", "Rue des Joffres 34",
+					20489, "Marseille", "France");
+			Client client3 = new Client("James Houston", "New-Town Way 10",
+					10489, "Los Angeles", "USA");
 
 			// creates 3 products
 			Product product1 = new Product("ASUS 8.0", "Notebook", 1200.0);
-			Product product2 = new Product("Samsung Galaxy Tab 2 10.1", "Tablet", 340.0);
-			Product product3 = new Product("Seagate USB", "USB Stick - 8GB", 6.0);
+			Product product2 = new Product("Samsung Galaxy Tab 2 10.1",
+					"Tablet", 340.0);
+			Product product3 = new Product("Seagate USB", "USB Stick - 8GB",
+					6.0);
 
 			// creates the 3 shop managers for 3 purchases
 			ShopManagement manager1 = new ShopManagement(client1, product2, 2);
@@ -83,8 +80,7 @@ public class ShopManagementTest extends MyExcelFunction {
 			// creates an array to save the arrayList
 			ShopManagement[] managerListArray = ShopManagement
 					.ArrayList(managerList);
-			
-			
+
 			// creates 2 new sheets for clients and products
 			HSSFSheet sheetClients = book.createSheet("Clients");
 			HSSFSheet sheetProducts = book.createSheet("Products");
@@ -92,97 +88,106 @@ public class ShopManagementTest extends MyExcelFunction {
 			// creates rows and cells for our new sheets
 			HSSFRow rowClient, rowProduct;
 			HSSFCell cellClient, cellProduct;
-			
+
 			// writes in clients sheet
 			rowClient = sheetClients.createRow(1);
 			cellClient = rowClient.createCell(1);
 			cellClient.setCellValue("ID");
 			cellClient.setCellStyle(getMyDefaultStyle(book));
-			
+
 			cellClient = rowClient.createCell(2);
 			cellClient.setCellValue("Name");
 			cellClient.setCellStyle(getMyDefaultStyle(book));
-			
+
 			cellClient = rowClient.createCell(3);
 			cellClient.setCellValue("Street");
 			cellClient.setCellStyle(getMyDefaultStyle(book));
-			
+
 			cellClient = rowClient.createCell(4);
 			cellClient.setCellValue("Zipcode");
 			cellClient.setCellStyle(getMyDefaultStyle(book));
-			
+
 			cellClient = rowClient.createCell(5);
 			cellClient.setCellValue("City");
 			cellClient.setCellStyle(getMyDefaultStyle(book));
-			
+
 			cellClient = rowClient.createCell(6);
 			cellClient.setCellValue("Country");
 			cellClient.setCellStyle(getMyDefaultStyle(book));
-			
+
 			for (int i = 2; i < managerListArray.length + 2; i++) {
 				rowClient = sheetClients.createRow(i);
 				cellClient = rowClient.createCell(1);
 				setMyCellAlignment(book, cellClient, defaultAlignment);
-				cellClient.setCellValue(managerListArray[i - 2].getClient().getId());
-				
+				cellClient.setCellValue(managerListArray[i - 2].getClient()
+						.getId());
+
 				cellClient = rowClient.createCell(2);
 				setMyCellAlignment(book, cellClient, defaultAlignment);
-				cellClient.setCellValue(managerListArray[i - 2].getClient().getName());
-				
+				cellClient.setCellValue(managerListArray[i - 2].getClient()
+						.getName());
+
 				cellClient = rowClient.createCell(3);
 				setMyCellAlignment(book, cellClient, defaultAlignment);
-				cellClient.setCellValue(managerListArray[i - 2].getClient().getStreet());
-				
+				cellClient.setCellValue(managerListArray[i - 2].getClient()
+						.getStreet());
+
 				cellClient = rowClient.createCell(4);
 				setMyCellAlignment(book, cellClient, defaultAlignment);
-				cellClient.setCellValue(managerListArray[i - 2].getClient().getZipcode());
-				
+				cellClient.setCellValue(managerListArray[i - 2].getClient()
+						.getZipcode());
+
 				cellClient = rowClient.createCell(5);
 				setMyCellAlignment(book, cellClient, defaultAlignment);
-				cellClient.setCellValue(managerListArray[i - 2].getClient().getCity());
-				
+				cellClient.setCellValue(managerListArray[i - 2].getClient()
+						.getCity());
+
 				cellClient = rowClient.createCell(6);
 				setMyCellAlignment(book, cellClient, defaultAlignment);
-				cellClient.setCellValue(managerListArray[i - 2].getClient().getCountry());
+				cellClient.setCellValue(managerListArray[i - 2].getClient()
+						.getCountry());
 			}
-			
+
 			// writes in products sheet
 			rowProduct = sheetProducts.createRow(1);
 			cellProduct = rowProduct.createCell(1);
 			cellProduct.setCellValue("Name");
 			cellProduct.setCellStyle(getMyDefaultStyle(book));
-			
+
 			cellProduct = rowProduct.createCell(2);
 			cellProduct.setCellValue("Description");
 			cellProduct.setCellStyle(getMyDefaultStyle(book));
-			
+
 			cellProduct = rowProduct.createCell(3);
 			cellProduct.setCellValue("Price");
 			cellProduct.setCellStyle(getMyDefaultStyle(book));
-			
+
 			for (int i = 2; i < managerListArray.length + 2; i++) {
 				rowProduct = sheetProducts.createRow(i);
 				cellProduct = rowProduct.createCell(1);
 				setMyCellAlignment(book, cellProduct, defaultAlignment);
-				cellProduct.setCellValue(managerListArray[i - 2].getProduct().getName());
+				cellProduct.setCellValue(managerListArray[i - 2].getProduct()
+						.getName());
 
 				cellProduct = rowProduct.createCell(2);
 				setMyCellAlignment(book, cellProduct, defaultAlignment);
-				cellProduct.setCellValue(managerListArray[i - 2].getProduct().getDesc());
-				
+				cellProduct.setCellValue(managerListArray[i - 2].getProduct()
+						.getDesc());
+
 				cellProduct = rowProduct.createCell(3);
 				setMyCellAlignment(book, cellProduct, defaultAlignment);
-				cellProduct.setCellValue(managerListArray[i - 2].getProduct().getPrice());				
+				cellProduct.setCellValue(managerListArray[i - 2].getProduct()
+						.getPrice());
 			}
-			
 
 			// 1st row
 			rowIndex++;
 			row = sheet.createRow(rowIndex);
 
 			// saves the header's title
-			String[] headerTitle = { "Client's ID", "Client", "Delivery address", "Article",
-					"Price / article", "Description", "Qty", "Total" };
+			String[] headerTitle = { "Client's ID", "Client",
+					"Delivery address", "Article", "Price / article",
+					"Description", "Qty", "Total" };
 
 			// writes header's title into the row
 			for (int i = 0; i < headerTitle.length; i++) {
@@ -204,7 +209,7 @@ public class ShopManagementTest extends MyExcelFunction {
 				cell = row.createCell(1);
 				cell.setCellValue(managerListArray[i].getClient().getId());
 				setMyCellAlignment(book, cell, defaultAlignment);
-				
+
 				cell = row.createCell(2);
 				cell.setCellValue(managerListArray[i].getClient().getName());
 				setMyCellAlignment(book, cell, defaultAlignment);
@@ -260,17 +265,17 @@ public class ShopManagementTest extends MyExcelFunction {
 			FileOutputStream output = null;
 			try {
 				output = new FileOutputStream(FILE_NAME);
-				
+
 				// writes the workbook into the FileOutputStream
 				book.write(output);
-				
+
 				// closes the FileOutputStream to avoid any other modifications
 				output.close();
 
 				// confirms the creation of the sheet through the console
 				System.out.println("The spreadsheet <" + FILE_NAME
 						+ "> has been successfull created.");
-				
+
 			} catch (FileNotFoundException e) {
 				e.printStackTrace();
 			} catch (IOException e) {
