2d0bf3e55d7f877e6ecfb18e22a99aaf9c6f542b
Marius Guede <mrius>
2013-04-12 13:09:53 +0200

    pull master

--- xlseasy/xlseasy.impl/src/main/java/org/adorsys/xlseasy/impl/proc/WorkbookDescFactory.java
+++ xlseasy/xlseasy.impl/src/main/java/org/adorsys/xlseasy/impl/proc/WorkbookDescFactory.java
@@ -4,7 +4,6 @@ import org.adorsys.xlseasy.annotation.Workbook;
 import org.adorsys.xlseasy.annotation.filter.AnnotationUtil;
 
 
-
 /**
  * Will be used to create workbook descriptors. This will provide the 
  * opportunity to inject other way of parsing class. In particular
