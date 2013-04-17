a0fe98ca5726c78e7270c0dcf6239d759b9525ec
Marius Guede <mrius>
2013-04-17 11:56:24 +0200

    update xlseasy.self

--- xlseasy.self/src/test/java/org/adorsys/xlseasy/self/Product.java
+++ xlseasy.self/src/test/java/org/adorsys/xlseasy/self/Product.java
@@ -1,19 +1,19 @@
 package org.adorsys.xlseasy.self;
 
 /**
- * The Class Product.
- * 
- * @author mariusguede
+ * The class Product.
+ *
+ * @author Marius Guede
  */
 public class Product {
 
-    /** Product's name. */
+	/** The product's name. */
     private String name;
 
-    /** Product's description. */
+	/** The product's description. */
     private String description;
 
-    /** Product's price. */
+	/** The product's price. */
     private Double price;
 
     /**
@@ -37,57 +37,57 @@ public class Product {
         this.price = price;
     }
 
-	/**
-	 * Gets the product's name.
-	 *
-	 * @return the product's name
-	 */
-	public String getName() {
-		return name;
-	}
+    /**
+     * Gets the product's name.
+     *
+     * @return the product's name
+     */
+    public String getName() {
+        return name;
+    }
 
-	/**
-	 * Sets the product's name.
-	 *
-	 * @param name the new product's name
-	 */
-	public void setName(String name) {
-		this.name = name;
-	}
+    /**
+     * Sets the product's name.
+     *
+     * @param name the new product's name
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
 
-	/**
-	 * Gets the product's description.
-	 *
-	 * @return the product's description
-	 */
-	public String getDescription() {
-		return description;
-	}
+    /**
+     * Gets the product's description.
+     *
+     * @return the product's description
+     */
+    public String getDescription() {
+        return description;
+    }
 
-	/**
-	 * Sets the product's description.
-	 *
-	 * @param description the new product's description
-	 */
-	public void setDescription(String description) {
-		this.description = description;
-	}
+    /**
+     * Sets the product's description.
+     *
+     * @param description the new product's description
+     */
+    public void setDescription(String description) {
+        this.description = description;
+    }
 
-	/**
-	 * Gets the product's price.
-	 *
-	 * @return the product's price
-	 */
-	public Double getPrice() {
-		return price;
-	}
+    /**
+     * Gets the product's price.
+     *
+     * @return the product's price
+     */
+    public Double getPrice() {
+        return price;
+    }
 
-	/**
-	 * Sets the product's price.
-	 *
-	 * @param price the new product's price
-	 */
-	public void setPrice(Double price) {
-		this.price = price;
-	}
+    /**
+     * Sets the product's price.
+     *
+     * @param price the new product's price
+     */
+    public void setPrice(Double price) {
+        this.price = price;
+    }
 }
--- xlseasy.self/src/test/java/org/adorsys/xlseasy/self/ShopManagement.java
+++ xlseasy.self/src/test/java/org/adorsys/xlseasy/self/ShopManagement.java
@@ -4,53 +4,60 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * The class ShopManagement
- *  
- * @author mariusguede
+ * This is the mapping of an excel file. Each collection field 
+ * is an excel-sheet. Non collection fields are ignored.
+ * 
+ * Getters and setters are required. Sheet can be lists or sets.
+ * 
+ * Sheet are processed in the order of the fields. If this class extends
+ * another one, sheets of this class are process first, then sheet from
+ * other classes are processed.
+ * 
+ * In any case, the Processing Interface allow the specifications
+ * 
+ * @author Sandro Sonntag
+ * @author Francis Pouatcha
+ * @author Marius Guede
  */
 public class ShopManagement {
 	
-	/**
-	 * The products
-	 */
+	/** Sheet product. */
 	private List<Product> products = new ArrayList<Product>();
 	
-	/**
-	 * The clients
-	 */
+	/** Sheet suppliers. */
 	private List<Client> clients = new ArrayList<Client>();
 	
 	/**
-	 * Gets the products.
+	 * Gets the sheet product.
 	 *
-	 * @return the products
+	 * @return the sheet product
 	 */
 	public List<Product> getProducts() {
 		return products;
 	}
 
 	/**
-	 * Sets the clients.
+	 * Sets the sheet suppliers.
 	 *
-	 * @param client the new clients
+	 * @param clients the new sheet suppliers
 	 */
 	public void setClients(List<Client> clients) {
 		this.clients = clients;
 	}
 
 	/**
-	 * Gets the clients.
+	 * Gets the sheet suppliers.
 	 *
-	 * @return the clients
+	 * @return the sheet suppliers
 	 */
 	public List<Client> getClients() {
 		return clients;
 	}
 
 	/**
-	 * Sets the products.
+	 * Sets the sheet product.
 	 *
-	 * @param products the new products
+	 * @param products the new sheet product
 	 */
 	public void setProducts(List<Product> products) {
 		this.products = products;
